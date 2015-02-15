47d5abe2f74990650b6e14a960cd6296a8a869d0
Kim Syversen <kim.syversen@gmail.com>
2015-02-15 17:03:52 +0100

    Save

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ use Uninett\Collections\CollectionUpdateInterface;
 require 'start/bootstrap.php';
 
 $collections = [
-	//new \Uninett\Collections\Users\UserImport(),
-	//new \Uninett\Collections\Users\UserCheckStatus(),
-	//new \Uninett\Collections\Users\UserSetAffiliation(),
-	new \Uninett\Collections\Presentations\PresentationImport(false)
+	new \Uninett\Collections\Users\UserImport,
+	new \Uninett\Collections\Users\UserSetAffiliation,
+	new \Uninett\Collections\Users\UserCheckStatus,
+	new \Uninett\Collections\UserDiskusage\UserDiskUsage,
+	new \Uninett\Collections\Presentations\PresentationImport(false),
+	new \Uninett\Collections\Org\OrgImport,
+
 ];
 
 /* @var $collection CollectionUpdateInterface */
