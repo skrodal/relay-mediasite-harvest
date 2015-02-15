de0ed0f8dfb52bbccd6dc9f6ea70922e30dac5f8
Kim Syversen <kim.syversen@gmail.com>
2015-02-15 15:12:41 +0100

    Save

--- index.php
+++ index.php
@@ -3,15 +3,13 @@ use Uninett\Collections\CollectionUpdateInterface;
 
 require 'start/bootstrap.php';
 
-
-
 $collections = [
-	new \Uninett\Collections\Users\UserImport(),
-	new \Uninett\Collections\Users\UserCheckStatus(),
-	new \Uninett\Collections\Users\UserSetAffiliation(),
+	//new \Uninett\Collections\Users\UserImport(),
+	//new \Uninett\Collections\Users\UserCheckStatus(),
+	//new \Uninett\Collections\Users\UserSetAffiliation(),
 ];
 
 /* @var $collection CollectionUpdateInterface */
-foreach($collections as $collection)
-	$collection->update();
+//foreach($collections as $collection)
+//	$collection->update();
 
--- start/bootstrap.php
+++ start/bootstrap.php
@@ -6,6 +6,6 @@ $basePath = dirname('../'. __FILE__);
 /**
  * Autoload third party modules
  */
-require dirname(__FILE__) . '/../vendor/autoload.php';
-require dirname(__FILE__) . '/../config.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/config.php';
 
