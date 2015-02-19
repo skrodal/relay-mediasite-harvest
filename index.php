6924ff3c7cd15f39a84003a6a80ee0704254921f
Kim Syversen <kim.syversen@gmail.com>
2015-02-19 21:55:36 +0100

    Save

--- index.php
+++ index.php
@@ -4,22 +4,15 @@ require 'start/bootstrap.php';
 use Uninett\Collections\CollectionUpdateInterface;
 
 $collections = [
-	new \Uninett\Collections\Presentations\PresentationCheckForDeleted
-/*	new \Uninett\Collections\Users\UserImport,
-	new \Uninett\Collections\Users\UserSetAffiliation,
-	new \Uninett\Collections\Users\UserCheckStatus,
-	new \Uninett\Collections\Presentations\PresentationImport(false),
-	new \Uninett\Collections\Presentations\PresentationCheckForDeleted*/
-
-/*	new \Uninett\Collections\Users\UserImport,
+	new \Uninett\Collections\Users\UserImport,
 	new \Uninett\Collections\Users\UserSetAffiliation,
 	new \Uninett\Collections\Users\UserCheckStatus,
 	new \Uninett\Collections\UserDiskusage\UserDiskUsageImport,
-	new \Uninett\Collections\Presentations\PresentationImport(false),
+	new \Uninett\Collections\Presentations\PresentationImport(true),
 	new \Uninett\Collections\Org\OrgImport,
 	new \Uninett\Collections\Org\OrgAggregateSizeUsed,
 	new \Uninett\Collections\Mediasite\MediasiteAggregateSizeUsed,
-	new \Uninett\Collections\Presentations\PresentationCheckForDeleted*/
+	new \Uninett\Collections\Presentations\PresentationCheckForDeleted
 ];
 
 /* @var $collection CollectionUpdateInterface */
