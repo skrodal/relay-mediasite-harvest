6e1bc70c561f72ac45556e975de663aa79ba3800
Kim Syversen <kim.syversen@gmail.com>
2015-02-20 07:02:30 +0100

    Code improvements

--- App/Collections/Mediasite/MediasiteAggregateSizeUsed.php
+++ App/Collections/Mediasite/MediasiteAggregateSizeUsed.php
@@ -16,8 +16,6 @@ use Uninett\Schemas\MediaSiteSchema;
  */
 class MediasiteAggregateSizeUsed extends Collection implements CollectionUpdateInterface
 {
-    private $criteria = array();
-
 	private $numberInserted = 0;
 
     public function __construct()
@@ -25,23 +23,10 @@ class MediasiteAggregateSizeUsed extends Collection implements CollectionUpdateI
 	    parent::__construct(MediaSiteSchema::COLLECTION_NAME);
 
         $this->mongo = new MongoConnection(MediaSiteSchema::COLLECTION_NAME);
-
-	    //TODO: Tror dette kan fjernes. Sjekk mongospec (update, upsert)
-        //Check if collection exists, if not, create it
-/*        if (!$this->collectionExists(MediaSiteSchema::COLLECTION_NAME)) {
-
-            $this->insert();
-
-            $this->criteria = array(MediaSiteSchema::ORG => "Dummy");
-
-            //Then remove dummy
-            $this->mongo->collection->remove($this->criteria);
-        }*/
     }
 
     public function update()
     {
-	    //TODO: Bruk array og gå gjennom alle potensielle directories for mediasite
         $directory = "/home/uninett/mediasite/";
 
         $lop = new LinuxOperationsHelper();
@@ -49,8 +34,6 @@ class MediasiteAggregateSizeUsed extends Collection implements CollectionUpdateI
         $organisations = $lop->getFolderNamesFromDirectory($directory);
 
         foreach ($organisations as $organisation) {
-            $lop = new LinuxOperationsHelper();
-
             $sizeB = $lop->getSpaceUsedByMediasiteOrg($directory, $organisation);
 
             $convert = new ConvertHelper();
@@ -75,46 +58,10 @@ class MediasiteAggregateSizeUsed extends Collection implements CollectionUpdateI
                     $this->numberInserted = $this->numberInserted + 1;
 
             } else
-                $this->LogError("Could not find " . $directory.DIRECTORY_SEPARATOR.$organisation);
+                $this->LogError("Could not find {$directory}/{$organisation}");
         }
-
 
         $this->LogInfo("Aggregated data and inserted {$this->numberInserted} items");
 
     }
-
-/*    public function collectionExists($collection)
-    {
-	    $config = Config::get('mongoDatabase');
-
-        if ($this->mongo->collection->system->namespaces->findOne(array("name" => $config['db'] .".".$collection)) === null)
-            return false;
-        return true;
-    }*/
-
-    /**
-     * Use only first time system is set up. This creates the collection and document
-     * @return array|bool
-     */
-/*    public function insert()
-    {
-        $cursor = $this->mongo->findLimitOneCount($this->criteria);
-
-        if (empty($cursor)) {
-            $success =  $this->mongo->insert
-            (
-                array
-                (
-                    MediaSiteSchema::ORG => "Dummy",
-                    MediaSiteSchema::STORAGE => array (),
-                )
-            );
-            if($success)
-                $this->LogInfo("Created collection {MediaSiteSchema::COLLECTION_NAME}");
-
-            return $success;
-        }
-
-        return false;
-    }*/
 }
