fde45a6272019cf37cadf64c56bdf862d6e7db5b
Kim Syversen <kim.syversen@gmail.com>
2015-02-25 17:19:00 +0100

    Save

--- App/Collections/RequestPerHour/RequestPerHourImport.php
+++ App/Collections/RequestPerHour/RequestPerHourImport.php
@@ -58,6 +58,8 @@ abstract class RequestPerHourImport extends Collection implements UpdateInterfac
 
 		$query = $find->findData($date);
 
+		$this->LogInfo("Trying to find data for date {$date->format('Y-m-d H:i:s')}");
+
 		if ($this->queryContainsNewFiles($query)) {
 
 			while ($result = mssql_fetch_assoc($query)) {
