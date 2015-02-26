a81ccc38230ade33618985e3a750992b41dcf0f0
Kim Syversen <kim.syversen@gmail.com>
2015-02-26 07:16:26 +0100

    Save

--- App/Schemas/RequestsPerHourSchema.php
+++ App/Schemas/RequestsPerHourSchema.php
@@ -4,7 +4,7 @@ class RequestsPerHourSchema
     const COLLECTION_NAME = "requestsPerHour";
     const PICTOR_TABLE_NAME = "requestsPerHour";
     const DATE = "date";
-    const REQUEST = "request";
-    const BYTES_SENT = "bytesSent";
+    const REQUEST = "requests";
+    //const BYTES_SENT = "bytesSent";
 
 }
